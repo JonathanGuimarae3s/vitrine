deb6f4e01d3b654716d0efaa17b68179a0b5de22
Jonathan Guimarães <92338228+JonathanGuimarae3s@users.noreply.github.com>
2021-10-19 19:30:03 -0300

    atualizado

--- freecs.js
+++ freecs.js
@@ -3,45 +3,45 @@
 function veste0(){
     var veste0 = document.querySelectorAll('span.nomevest')[0]
     var dressname  = veste0.innerHTML
-    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá%20queria%20saber%20mais%20sobre%20esse vestido%20${dressname}%20e%20como%20voce%20trabalha.`) 
+    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá,%20queria%20saber%20mais%20sobre%20esse%20vestido%20${dressname}%20e%20como%20você%20trabalha.`) 
     
   
 }
 function veste1(){
     var veste1 = document.querySelectorAll('.nomevest') [1]
     var dressname = veste1.innerHTML
-    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá%20queria%20saber%20mais%20sobre%20esse vestido%20${dressname}%20e%20como%20voce%20trabalha.`)
+    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá,%20queria%20saber%20mais%20sobre%20esse%20vestido%20${dressname}%20e%20como%20você%20trabalha.`)
     
 }
 function veste2(){
     var veste2 = document.querySelectorAll('span.nomevest')[2]
     var dressname  = veste2.innerHTML
-    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá%20queria%20saber%20mais%20sobre%20esse vestido%20${dressname}%20e%20como%20voce%20trabalha.`) 
+    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá,%20queria%20saber%20mais%20sobre%20esse vestido%20${dressname}%20e%20como%20você%20trabalha.`) 
     
   
 }
 function veste3(){
     var veste3 = document.querySelectorAll('.nomevest') [3]
     var dressname = veste3.innerHTML
-    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá%20queria%20saber%20mais%20sobre%20esse vestido%20${dressname}%20e%20como%20voce%20trabalha.`)
+    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá,%20queria%20saber%20mais%20sobre%20esse%20vestido%20${dressname}%20e%20como%20você%20trabalha.`)
     
 }
 function veste4(){
     var veste4 = document.querySelectorAll('span.nomevest')[4]
     var dressname  = veste4.innerHTML
-    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá%20queria%20saber%20mais%20sobre%20esse vestido%20${dressname}%20e%20como%20voce%20trabalha.`) 
+    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá,%20queria%20saber%20mais%20sobre%20esse vestido%20${dressname}%20e%20como%20você%20trabalha.`) 
     
   
 }
 function veste5(){
     var veste5 = document.querySelectorAll('.nomevest') [5]
     var dressname = veste5.innerHTML
-    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá%20queria%20saber%20mais%20sobre%20esse vestido%20${dressname}%20e%20como%20voce%20trabalha.`)
+    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá,%20queria%20saber%20mais%20sobre%20esse%20vestido%20${dressname}%20e%20como%20você%20trabalha.`)
     
 }
 function veste6(){
     var veste6 = document.querySelectorAll('.nomevest') [6]
     var dressname = veste6.innerHTML
-    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá%20queria%20saber%20mais%20sobre%20esse vestido%20${dressname}%20e%20como%20você%20trabalha.`)
+    window.location.replace(`https://api.whatsapp.com/send?phone=+5521982441600&text=Olá,%20queria%20saber%20mais%20sobre%20esse%20vestido%20${dressname}%20e%20como%20você%20trabalha.`)
     
 }
