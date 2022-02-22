5907630559a1adae48ebc992e9bf2c7fd8d74c1a
JonathanGuimarae3s <guimaraesjonathan92@gmail.com>
2022-02-21 23:53:14 -0300

    script
    implementei o metodo open()

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 //pega o nome do vestido que foi clicado e manda por whatsapp
 
-var veste= document.querySelectorAll("span.nomevest");
-function vest(){
+var veste = document.querySelectorAll("span.nomevest");
 
-
-   window.location.replace(`https://api.whatsapp.com/send?phone=+5521965852442&text=Olá,%20queria%20saber%20mais%20sobre%20esse%20vestido%20${veste[0].innerHTML}%20e%20como%20você%20trabalha.`)
-   
-    
-  
-
-}function vest(){
-
-
-   window.location.replace(`https://api.whatsapp.com/send?phone=+5521965852442&text=Olá,%20queria%20saber%20mais%20sobre%20esse%20vestido%20${veste[1].innerHTML}%20e%20como%20você%20trabalha.`)
-   
-    
-  
-
+function vest() {
+	open(
+		`https://api.whatsapp.com/send?phone=+5521965852442&text=Olá,%20queria%20saber%20mais%20sobre%20esse%20vestido%20${veste[0].innerHTML}%20e%20como%20você%20trabalha.`
+	);
+   console.log(veste)
 }
